0929ccf26acfc1750c6bc9d217e332e8c01efde9
DanieleKun <pucciodaniele95@email.com>
2022-06-15 17:14:25 +0200

    prova stampa serie tv

---
+++ src/components/BfCardMovie.vue
@@ -0,0 +1,29 @@
+<template>
+     <div>
+        <h2>Titolo: {{moviesObject.title}}</h2>
+        <p>Titolo Originale: {{moviesObject.original_title}}</p>
+        <p>Lingua Originale: {{moviesObject.original_language}}
+        <lang-flag :iso="moviesObject.original_language"/>
+        </p>
+        <p>Voto: {{moviesObject.vote_average}}</p>
+    </div>
+</template>
+
+<script>
+import LangFlag from 'vue-lang-code-flags';
+
+export default {
+  name: 'BfCardMovie',
+  components: {
+    LangFlag,
+  },
+  props: {
+    moviesObject: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
